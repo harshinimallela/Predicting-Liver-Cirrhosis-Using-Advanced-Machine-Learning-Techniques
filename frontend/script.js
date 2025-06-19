3f9fdce91b02cb24453a91302c7bcc2fddab0106
teswarreddy <thathieswarreddy@gmail.com>
2025-06-19 21:17:02 +0530

    Add Dockerfile and updated app

--- frontend/script.js
+++ frontend/script.js
@@ -135,7 +135,7 @@ async function handleFormSubmit(event) {
     try {
         const formData = collectFormData();
         console.log(formData)
-        const response = await fetch('http://127.0.0.1:5000/api/predict', {
+        const response = await fetch('http://127.0.0.1:8000/api/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(formData)
